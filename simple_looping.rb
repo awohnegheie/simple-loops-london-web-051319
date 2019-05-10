2e10df1a45c2a45ed87123df2e24b88da330c1f6
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 02:17:21 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ def times_iterator(number_of_times)
   7.times do
      puts "Welcome to Flatiron School's Web Development Course!"
        phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+
 
 end
 
